ef023e719eba3ace28fc58bc1c3ad75621638343
Enrique Lozano <61509169+enriqueloz88@users.noreply.github.com>
2021-02-04 20:35:45 +0100

    Teams and attributes to df ✔️

--- main.py
+++ main.py
@@ -2,103 +2,126 @@ import csv
 import os
 import pandas as pd
 
-class Team:       
+class Team:   
+
     def __init__(self, name:str):           
-        self.name = name           
-        self.matches = 0
-        self.home_matches = 0
-        self.away_matches = 0
-        self.home_goals = 0
-        self.home_goals_against = 0
-        self.away_goals = 0
-        self.away_goals_against = 0
-        self.home_shots = 0
-        self.home_shots_against = 0
-        self.away_shots = 0
-        self.away_shots_against = 0
-        self.home_shotsT = 0
-        self.home_shotsT_against = 0
-        self.away_shotsT = 0
-        self.away_shotsT_against = 0
-        self.home_corners = 0
-        self.home_corners_against = 0
-        self.away_corners = 0
-        self.away_corners_against = 0
-        self.home_faults = 0
-        self.home_faults_against = 0
-        self.away_faults = 0
-        self.away_faults_against = 0
-        self.home_yellows = 0
-        self.home_yellows_against = 0
-        self.away_yellows = 0
-        self.away_yellows_against = 0
-        self.home_reds = 0
-        self.home_reds_against = 0
-        self.away_reds = 0
-        self.away_reds_against = 0
-        
-
-def main():
-    print ("WELCOME! This is my football data analizer.")
-    print ("--------------------")
-    print ("This code has been fully developed by enriqueloz88. Despite being an open source project, its use for commercial purposes without my express authorization is not allowed.")
-    print ("--------------------\n")
-
-    df = pd.read_csv('db/2020/E0.csv')
-
-    teams = df['HomeTeam'].unique()
-    teams.sort()
-
-    teamsObj = []
-
-    for team in teams:
-        t = Team(team)
-
-        for x in range(len(df.index)):
-            if team==df['HomeTeam'].values[x]:
-                t.matches = t.matches + 1
-                t.home_matches = t.home_matches + 1
-
-                t.home_goals = t.home_goals + df['FTHG'].values[x]
-                t.home_goals_against = t.home_goals_against + df['FTAG'].values[x]
-                t.home_shots = t.home_shots + df[''].values[x]
-                t.home_shots_against = t.home_shots_against + df[''].values[x]         
-                t.home_shotsT = t.home_shotsT + df[''].values[x]
-                t.home_shotsT_against = t.home_shotsT_against + df[''].values[x]             
-                t.home_corners = t.home_corners + df[''].values[x]
-                t.home_corners_against = t.home_corners_against + df[''].values[x]              
-                t.home_faults = t.home_faults + df[''].values[x]
-                t.home_faults_against = t.home_faults_against + df[''].values[x]              
-                t.home_yellows = t.home_yellows + df[''].values[x]
-                t.home_yellows_against = t.home_yellows_against + df[''].values[x]              
-                t.home_reds = t.home_reds + df[''].values[x]
-                t.home_reds_against = t.home_reds_against + df[''].values[x]
-                
-
-            if team==df['AwayTeam'].values[x]:
-                t.matches = t.matches + 1
-                t.away_matches = t.away_matches + 1
-                
-                t.away_goals = t.away_goals + df[''].values[x]
-                t.away_goals_against = t.away_goals_against + df[''].values[x]
-                t.away_shots = t.away_shots + df[''].values[x]
-                t.away_shots_against = t.away_shots_against + df[''].values[x]
-                t.away_shotsT = t.away_shotsT + df[''].values[x]
-                t.away_shotsT_against = t.away_shotsT_against + df[''].values[x]
-                t.away_corners = t.away_corners + df[''].values[x]
-                t.away_corners_against = t.away_corners_against + df[''].values[x]
-                t.away_faults = t.away_faults + df[''].values[x]
-                t.away_faults_against = t.away_faults_against + df[''].values[x]
-                t.away_yellows = t.away_yellows + df[''].values[x]
-                t.away_yellows_against = t.away_yellows_against + df[''].values[x]
-                t.away_reds = t.away_reds + df[''].values[x]
-                t.away_reds_against = t.away_reds_against + df[''].values[x]
-
-                        
+        self.Name = name
+        # ---- [H/A]X[F/A] -----
+        # H: Home, A: Away | F: For, A: Against
+        # ---------------------- 
+        self.HM = 0   # Matches
+        self.AM = 0
+        self.HGF = 0  # Goals
+        self.HGA = 0
+        self.AGF = 0
+        self.AGA = 0
+        self.HSF = 0  # Shots
+        self.HSA = 0
+        self.ASF = 0
+        self.ASA = 0
+        self.HSTF = 0 # Shots on Target 
+        self.HSTA = 0
+        self.ASTF = 0
+        self.ASTA = 0
+        self.HCF = 0  # Corners
+        self.HCA = 0
+        self.ACF = 0
+        self.ACA = 0
+        self.HFF = 0  # Faults
+        self.HFA = 0
+        self.AFF = 0
+        self.AFA = 0
+        self.HYF = 0  # Yellow cards
+        self.HYA = 0
+        self.AYF = 0
+        self.AYA = 0
+        self.HRF = 0  # Red cars
+        self.HRA = 0
+        self.ARF = 0
+        self.ARA = 0    
+    
+    # Convert to dict. 
+    # Return -> {'name': self.name, 'M': self.M,...}
+    def as_dict(self):
+        return self.__dict__ 
 
 
-        teamsObj.append(t)
+    # Dynamic parameters
+    def updateParameters(self):
+        self.M = self.HM + self.AM
+        self.GF = self.HGF + self.AGF
+        self.GA = self.HGA + self.AGA
+        self.SF = self.HSF + self.ASF
+        self.SA = self.HSA + self.ASA
+        self.STF = self.HSTF + self.ASTF
+        self.STA = self.HSTA + self.ASTA
+        self.CF = self.HCF + self.ACF
+        self.CA = self.HCA + self.ACA
+        self.FF = self.HFF + self.AFF
+        self.FA = self.HFA + self.AFA
+        self.YF = self.HYF + self.AYF
+        self.YA = self.HYA + self.AYA
+        self.RF = self.HRF + self.ARF
+        self.RA = self.HRA + self.ARA
 
-    print(teamsObj[1].home_matches)
 
-main()
+print ("WELCOME! This is my football data analizer.")
+print ("--------------------")
+print ("This code has been fully developed by enriqueloz88. Despite being an open source project, its use for commercial purposes without my express authorization is not allowed.")
+print ("--------------------\n")
+
+df = pd.read_csv('db/2020/E0.csv')
+
+teams = df['HomeTeam'].unique()
+teams.sort()
+
+teamsObj = []
+
+for team in teams:
+    t = Team(team)
+
+    for x in range(len(df.index)):
+        if team==df['HomeTeam'].values[x]:
+            t.HM = t.HM + 1
+            t.HGF = t.HGF + df['FTHG'].values[x]
+            t.HGA = t.HGA + df['FTAG'].values[x]
+            t.HSF = t.HSF + df['HS'].values[x]
+            t.HSA = t.HSA + df['AS'].values[x]         
+            t.HSTF = t.HSTF + df['HST'].values[x]
+            t.HSTA = t.HSTA + df['AST'].values[x]             
+            t.HCF = t.HCF + df['HC'].values[x]
+            t.HCA = t.HCA + df['AC'].values[x]              
+            t.HFF = t.HFF + df['HF'].values[x]
+            t.HFA = t.HFA + df['AF'].values[x]              
+            t.HYF = t.HYF + df['HY'].values[x]
+            t.HYA = t.HYA + df['AY'].values[x]              
+            t.HRF = t.HRF + df['HR'].values[x]
+            t.HRA = t.HRA + df['AR'].values[x]
+            t.updateParameters()
+            
+
+        if team==df['AwayTeam'].values[x]:
+            t.AM = t.AM + 1
+            t.AGF = t.AGF + df['FTAG'].values[x]
+            t.AGA = t.AGA + df['FTHG'].values[x]
+            t.ASF = t.ASF + df['AS'].values[x]
+            t.ASA = t.ASA + df['HS'].values[x]
+            t.ASTF = t.ASTF + df['AST'].values[x]
+            t.ASTA = t.ASTA + df['HST'].values[x]
+            t.ACF = t.ACF + df['AC'].values[x]
+            t.ACA = t.ACA + df['HC'].values[x]
+            t.AFF = t.AFF + df['AF'].values[x]
+            t.AFA = t.AFA + df['HF'].values[x]
+            t.AYF = t.AYF + df['AY'].values[x]
+            t.AYA = t.AYA + df['HY'].values[x]
+            t.ARF = t.ARF + df['AR'].values[x]
+            t.ARA = t.ARA + df['HR'].values[x]
+            t.updateParameters()       
+
+    teamsObj.append(t)
+
+df = pd.DataFrame([x.as_dict() for x in teamsObj])
+
+
+print(df)
+
